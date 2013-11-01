518b7764d755d5ceba3c6704776f5d6feb93a629
NGUYEN NGOC LAN <lan.startup.stanford@gmail.com>
2013-11-01 15:00:50 +0700

    Initialize demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '12cd7293b5b9f9539ca5807cd29c117fb2c354c3a3db4854c3c17c66242bf29afd9b1b34d2342cc48c2fa9d756e0dad0babacdfccf5e8d95427c199492805ac2'
